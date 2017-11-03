97047b095c4fdffebbc129e2378706855b463cd4
CrafterOfWorlds <33355500+CrafterOfWorlds@users.noreply.github.com>
2017-11-03 11:08:04 -0700

    Added cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SteamAuth;
+using System.IO.Ports;
+
+namespace SteamAuthConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SteamGuardAccount account = new SteamGuardAccount();
+            account.SharedSecret = args[0];
+            string loginCode = account.GenerateSteamGuardCode();
+            Console.Write(loginCode);
+        }
+    }
+}
